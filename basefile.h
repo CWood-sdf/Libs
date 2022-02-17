094d0ea85a5c43ad84a6abe072bc2f7c933cc5a6
CWood-sdf <98367120+CWood-sdf@users.noreply.github.com>
2022-02-16 22:17:05 -0500

    Added basefile

---
+++ basefile.h
@@ -0,0 +1,12 @@
+#include BASE/files.h>
+#include BASE/Button.h>
+#include BASE/GyroInit.h>
+//#include BASE/initialize.h>
+//#include BASE/APA_Init.h>
+#include BASE/PVector.h>
+#include BASE/Loader.h>
+#include BASE/macros.h>
+#include BASE/NewMotor.h>
+//#include BASE/Position.h>
+#include BASE/LinkedList.h>
+#include BASE/Matrix.h>
