2a1c94db46ec20159d56fbd6571cedac6649c97b
CWood-sdf <woodchristophermichael@gmail.com>
2022-09-26 15:59:16 -0400

    ssdsfd

--- LinkedList.h
+++ LinkedList.h
@@ -556,6 +556,8 @@ public:
 		}
 	}
 	void popBase() {
+		cout << "P" << base << endl;
+		cout << "Pn" << base->next << endl;
 		if (base == NULL) return;
 		if (base->next == NULL) {
 			Node* del = base;
