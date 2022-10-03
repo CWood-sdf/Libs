64b14a3e1a8b908577d5ce2c6c926366f1a56afb
CWood-sdf <woodchristophermichael@gmail.com>
2022-10-03 16:06:47 -0400

    stuff

--- Button.h
+++ Button.h
@@ -11,7 +11,7 @@ protected:
     int height;
     void (*function)(void);
     string label;
-    color fill;
+    const color& fill;
     color fill2 = color(50, 50, 50);
     int xOff = 0;
     int yOff = 0;
@@ -59,7 +59,7 @@ public:
     }
     void draw (){
       //fill(this.fill);
-      Brain.Screen.setFillColor(this->fill);
+      Brain.Screen.setFillColor(pressing() ? this->fill2 : this->fill);
       Brain.Screen.drawRectangle(x, y, width, height);
       
       Brain.Screen.setCursor(2, 1);
