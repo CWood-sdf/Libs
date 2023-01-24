1e1c2e3b3f19b3f143a4f95f5e289d97ae539979
CWood-sdf <woodchristophermichael@gmail.com>
2023-01-23 21:57:43 -0500

    cool

--- Loader.h
+++ Loader.h
@@ -2,13 +2,13 @@
 
 union PVector;
 #ifndef NO_MAKE
-void windowsLoader (bool){
+void windowsLoader(bool)
+{
     static PVector pos = PVector(0.0, 0.0);
     static PVector velocity = PVector(1.5, 1.5);
-  //while(1) {
     Brain.Screen.waitForRefresh();
     Brain.Screen.clearScreen(black);
-    
+
     Brain.Screen.drawRectangle(pos.x, pos.y, 50, 50, red);
     Brain.Screen.drawRectangle(pos.x, pos.y + 50, 50, 50, blue);
     Brain.Screen.drawRectangle(pos.x + 50, pos.y, 50, 50, green);
@@ -18,20 +18,23 @@ void windowsLoader (bool){
     Brain.Screen.drawRectangle(pos.x - 30, pos.y + 30, 20, 20, black);
     Brain.Screen.drawRectangle(pos.x + 10, pos.y + 30, 20, 20, black);*/
     pos.add(velocity);
-    if(pos.x + 100 > 480){
-      velocity.x *= -1;
+    if (pos.x + 100 > 480)
+    {
+        velocity.x *= -1;
     }
-    if(pos.y + 100 > 240){
-      velocity.y *= -1;
+    if (pos.y + 100 > 240)
+    {
+        velocity.y *= -1;
     }
-    if(pos.x < 0){
-      velocity.x *= -1;
+    if (pos.x < 0)
+    {
+        velocity.x *= -1;
     }
-    if(pos.y < 0){
-      velocity.y *= -1;
+    if (pos.y < 0)
+    {
+        velocity.y *= -1;
     }
-  //}
 }
-#else 
+#else
 void windowsLoader();
 #endif
